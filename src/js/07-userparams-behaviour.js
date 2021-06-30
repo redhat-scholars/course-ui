73a9e86e10896946c77f9d9b5cd92dc05e3e01a4
Marc Hildenbrand <mwhildenbrand@gmail.com>
2021-06-30 12:54:23 +0000

    Fix issues found by linter

--- src/js/07-userparams-behaviour.js
+++ src/js/07-userparams-behaviour.js
@@ -16,24 +16,24 @@ document.addEventListener('DOMContentLoaded', function () {
     allNavLinks.forEach(appendQueryStringToHref)
   }
 
-  function appendQueryStringToHref(el) {
+  function appendQueryStringToHref (el) {
     var desiredQueryString = new URLSearchParams(window.location.search)
     var appendQueryString = el.classList.contains('query-params-link') ||
       el.classList.contains('nav-link')
 
     if (desiredQueryString.toString() && appendQueryString) {
-      var hrefURL = new URL(el.href);
+      var hrefURL = new URL(el.href)
       for (var k of desiredQueryString.keys()) {
-        hrefURL.searchParams.append(k, desiredQueryString.get(k));
+        hrefURL.searchParams.append(k, desiredQueryString.get(k))
       }
 
-      el.href = hrefURL.toString();
+      el.href = hrefURL.toString()
     }
   }
 
   // refreshing links
 
-  function replaceParamsInNodes(node, key, value) {
+  function replaceParamsInNodes (node, key, value) {
     if (node.parentElement) {
       //console.log('Parent element %s', node.parentElement.nodeName)
       if (node.parentElement.nodeName === 'code' ||
@@ -52,19 +52,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // handle link elements
       if (node.href) {
-        node.href = applyPattern(node.href, key, value);
+        node.href = applyPattern(node.href, key, value)
       }
     }
   }
 
-  // If there are query parameters (searchparams) in the current window location then 
-  // Iterate over all them replacing text and link-hrefs that contain them
-  var params = new URLSearchParams(window.location.search);
+  // If there are query parameters (searchparams) in the current window location
+  // then iterate over all them replacing text and link-hrefs that contain them
+  var params = new URLSearchParams(window.location.search)
   for (var k of params.keys()) {
-    replaceParamsInNodes(document.body, k, params.get(k));
+    replaceParamsInNodes(document.body, k, params.get(k))
   }
 
-  function applyPattern(str, key, value) {
+  function applyPattern (str, key, value) {
     //(%25key%25|%key%) %25 is urlencode value of %
     var pattern = '(' + '%25' + key + '%25' +
       '|(?<!-)' + '%' + key + '%' + '(?!-))'
@@ -72,4 +72,4 @@ document.addEventListener('DOMContentLoaded', function () {
     return str.replace(re, value)
   }
 
-});
+})
